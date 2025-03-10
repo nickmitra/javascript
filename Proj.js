c3e264233af246f5d7cae94f77d27b7dc3785413
Sagnik Mitra <159164064+nickmitra@users.noreply.github.com>
2025-03-10 22:10:09 +0530

    Add files via upload

---
+++ Proj.js
@@ -0,0 +1,11 @@
+let variable ="35";
+//cannot acess before intialization = hosting
+//exponentiation in js is **
+//strict equal to -> === ; value and data type !== is strict not eual to
+//Document Object Manipulation ;prog interface for web docs ;elements , attributes , nodes
+//DOM getELementById(id), getElementsByClassName() , getElementsByTagNAme()
+//  //querySelector(selector)// querySelectorAll(selector
+// ) selects the first element that matches the specified css selector
+//set styles using classList Method classlIst.add(),.remove().toggle()
+console.log(typeof null)
+//in js , '$'+20+2.3 would give $202.5 as left->right
